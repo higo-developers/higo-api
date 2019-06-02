1ebf06bc5a8f2e181cd41023fdf45a268aef3635
DEMIAN DANIEL BACIGALUPO <mercenary666_12@hotmail.copm>
2019-06-02 13:20:41 -0300

    Wip Listado Vehiculos

---
+++ HigoApi/Services/IVehiculoService.cs
@@ -0,0 +1,13 @@
+﻿using HigoApi.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HigoApi.Services
+{
+    interface IVehiculoService
+    {
+        public List<VehiculoResponse> Listar();
+    }
+}
