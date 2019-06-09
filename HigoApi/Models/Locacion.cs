f15f3055f382fe71d572334ea384c86e55bc7f10
Pablo Bertoli <berto2904@gmail.com>
2019-06-09 19:57:21 -0300

    addMigration de Locacion

--- HigoApi/Models/Locacion.cs
+++ HigoApi/Models/Locacion.cs
@@ -14,6 +14,10 @@ namespace HigoApi.Models
         public int IdLocacion { get; set; }
         public string Latitud { get; set; }
         public string Longitud { get; set; }
+        public string Pais { get; set; }
+        public string Provincia{ get; set; }
+        public string Partido { get; set; }
+        public string Localidad { get; set; }
 
         public virtual ICollection<Usuario> Usuario { get; set; }
         public virtual ICollection<Vehiculo> Vehiculo { get; set; }
