f1a594ddb5ec0081d2ef71f560b8415100d6ee44
Santi Leiva <santiagoeleiva@gmail.com>
2019-06-11 15:57:42 -0300

    Se agrega OperacionUtils

---
+++ HigoApi/Utils/OperacionUtils.cs
@@ -0,0 +1,21 @@
+using System;
+using HigoApi.Models;
+
+namespace HigoApi.Utils
+{
+    public static class OperacionUtils
+    {
+        public static bool BetweenDateTimes(Operacion operacion, DateTime fechaDesde, DateTime fechaHasta)
+        {
+            var rangoBusquedaEntreRangoOperacion =
+                operacion.FechaHoraDesde <= fechaDesde && fechaDesde <= operacion.FechaHoraHasta
+                || operacion.FechaHoraDesde <= fechaHasta && fechaHasta <= operacion.FechaHoraHasta;
+
+            var rangoOperacionEntreRangoBusqueda =
+                fechaDesde <= operacion.FechaHoraHasta && operacion.FechaHoraHasta <= fechaHasta
+                || fechaDesde <= operacion.FechaHoraDesde && operacion.FechaHoraHasta <= fechaHasta;
+
+            return rangoBusquedaEntreRangoOperacion || rangoOperacionEntreRangoBusqueda;
+        }
+    }
+}
