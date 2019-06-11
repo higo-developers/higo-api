5a601004efcd07a439c602032acb14f6d092b609
Pablo Bertoli <berto2904@gmail.com>
2019-06-10 23:28:57 -0300

    Error response

---
+++ HigoApi/Models/DTO/ErrorResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HigoApi.Models.DTO
+{
+    public class ErrorResponse
+    {
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public ErrorResponse(int code, string errorMessage)
+        {
+            ErrorCode = code;
+            ErrorMessage = errorMessage;
+        }
+    }
+}
