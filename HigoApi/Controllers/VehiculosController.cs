66f272b0252848ed1d9fe325b5243e47448a6642
Santi Leiva <santiagoeleiva@gmail.com>
2019-06-10 23:19:50 -0300

    Se agrega validación de parámetros en VehiculosController

--- HigoApi/Controllers/VehiculosController.cs
+++ HigoApi/Controllers/VehiculosController.cs
@@ -1,5 +1,9 @@
+using System;
+using System.ComponentModel.DataAnnotations;
 using HigoApi.Models.DTO;
 using HigoApi.Services;
+using HigoApi.Validators;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HigoApi.Controllers
@@ -9,15 +13,38 @@ namespace HigoApi.Controllers
     public class VehiculosController : ControllerBase
     {
         private readonly IVehiculoService vehiculoService;
+        private readonly ParametrosBusquedaVehiculoValidator parametrosValidator;
 
-        public VehiculosController(IVehiculoService vehiculoService)
+        public VehiculosController(IVehiculoService vehiculoService,
+            ParametrosBusquedaVehiculoValidator parametrosValidator)
         {
             this.vehiculoService = vehiculoService;
+            this.parametrosValidator = parametrosValidator;
         }
 
         [HttpGet]
         public IActionResult Get([FromQuery] ParametrosBusquedaVehiculo parametros)
         {
+            try
+            {
+                parametrosValidator.Validate(parametros);
+            }
+            catch (ValidationException ve)
+            {
+                Console.WriteLine(ve);
+                return BadRequest(ve.Message);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe);
+                return UnprocessableEntity(fe.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+
             return Ok(vehiculoService.Listar(parametros));
         }
     }
