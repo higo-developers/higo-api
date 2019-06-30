686d406c21c3f7c92e04d178a58128369042e51d
Santi Leiva <santiagoeleiva@gmail.com>
2019-06-30 16:50:09 -0300

    Agreaga PerfilVehiculoDTO

---
+++ HigoApi/Models/DTO/PerfilVehiculoDTO.cs
@@ -0,0 +1,30 @@
+namespace HigoApi.Models.DTO
+{
+    public class PerfilVehiculoDTO
+    {
+        public int Id { get; set; }
+        
+        public string Estado { get; set; }
+        public string Tipo { get; set; }
+        public string Combustible { get; set; }
+        public string Patente { get; set; }
+        public string Anno { get; set; }
+        
+        public int Modelo { get; set; }
+        public int Cilindrada { get; set; }
+        
+        // EQUIPAMIENTO
+        
+        public bool AC { get; set; }
+        public bool DA { get; set; }
+        public bool DH { get; set; }
+        public bool Alarma { get; set; }
+        public bool CierreCentralizado { get; set; }
+        public bool RompenieblasDelantero { get; set; }
+        public bool RompenieblasTrasero { get; set; }
+        public bool Airbag { get; set; }
+        public bool ABS { get; set; }
+        public bool ControlTraccion { get; set; }
+        public bool TapizadoCuero { get; set; }
+    }
+}
